7e8024d6da5a4a38675815558b51845ebfe49ca6
Sachin <mittalsam98@gmail.com>
2023-12-25 22:11:12 +0530

    Schema updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { type Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
-    },
+        sans: ['var(--font-sans)', ...fontFamily.sans]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 } satisfies Config;
