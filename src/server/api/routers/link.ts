be2c388f574c5472fd7cde0dd805a8e3c62781dc
Sachin <mittalsam98@gmail.com>
2024-01-13 23:38:05 +0530

    Added getUser function and update schema

--- src/server/api/routers/link.ts
+++ src/server/api/routers/link.ts
@@ -1,15 +1,38 @@
-import { createTRPCRouter, protectedProcedure, publicProcedure } from '@/server/api/trpc';
+import { Context, createTRPCRouter, protectedProcedure, publicProcedure } from '@/server/api/trpc';
 import { UpdateLinkSchema } from '../schemas/links';
 import { z } from 'zod';
+import { db } from '@/server/db';
+
+const getUser = async (ctx: Context) => {
+  const { session } = ctx;
+
+  const user = await db.user.findUnique({
+    where: {
+      id: session?.user.id
+    },
+    select: {
+      id: true,
+      email: true,
+      name: true
+    }
+  });
+
+  if (!user) throw new Error('User not found');
+
+  return user;
+};
 
 export const linkRouter = createTRPCRouter({
-  updateProfile: publicProcedure.input(UpdateLinkSchema).mutation(async ({ input }) => {
-    console.log(input, 'sas');
-    return;
-  }),
-  getCookie: publicProcedure.input(z.string()).query(async ({ input }) => {
-    console.log(input, 'sdas');
-
-    return '🍪';
+  updateProfile: protectedProcedure.input(UpdateLinkSchema).mutation(async ({ input, ctx }) => {
+    const { session } = ctx;
+    const user = await getUser(ctx);
+    await db.userProfile.create({
+      data: {
+        title: input.title,
+        bio: input.bio,
+        userId: user.id
+      }
+    });
+    return user;
   })
 });
