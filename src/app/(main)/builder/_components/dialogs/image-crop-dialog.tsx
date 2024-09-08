0f9ce557a3bbb63133b527de44283c121e93296a
Sachin <mittalsam98@gmail.com>
2024-09-08 22:55:18 +0530

    Fix build fail issue in cropper

--- src/app/(main)/builder/_components/dialogs/image-crop-dialog.tsx
+++ src/app/(main)/builder/_components/dialogs/image-crop-dialog.tsx
@@ -8,11 +8,15 @@ import 'react-image-crop/dist/ReactCrop.css';
 import setCanvasPreview from '../appearance/profile-section/set-canvas-preview';
 
 function dataURLtoBlob(dataurl: string) {
-  var arr = dataurl.split(','),
-    mime = arr[0].match(/:(.*?);/)[1],
-    bstr = atob(arr[1]),
-    n = bstr.length,
-    u8arr = new Uint8Array(n);
+  const arr = dataurl.split(',');
+  const match = arr[0]?.match(/:(.*?);/);
+  const mime = match ? match[1] : ''; // Fallback to an empty string if match is null
+  if (!arr[1]) {
+    throw new Error('Invalid data URL'); // Or handle it another way
+  }
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
   while (n--) {
     u8arr[n] = bstr.charCodeAt(n);
   }
